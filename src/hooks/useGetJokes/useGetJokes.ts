a3934876f83267c04b3075f8ec34ba373446bd8d
Thavin Voeun - KHM <ThavinV@th.fwd.com>
2022-04-20 09:28:00 +0700

    first commit

---
+++ src/hooks/useGetJokes/useGetJokes.ts
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+
+export const useGetJokes = (): [
+  () => Promise<void>,
+  string[],
+  boolean,
+  string
+] => {
+  const [joke, setJoke] = useState([""]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+  const endpoint =
+    "https://sv443.net/jokeapi/v2/joke/Any?blacklistFlags=racist&type=single";
+
+  const getJoke = async () => {
+    setIsLoading(true);
+    try {
+      let res = await fetch(endpoint);
+      let data = await res.json();
+      const dataArr = data.joke.split("\n");
+      setJoke(dataArr);
+      if (error) setError("");
+      setIsLoading(false);
+    } catch (err: any) {
+      setError(err.message);
+      setIsLoading(false);
+    }
+  };
+
+  // Fetch data on first mount
+  useEffect(() => {
+    getJoke();
+  }, []);
+
+  return [getJoke, joke, isLoading, error];
+};
---
+++ src/ui/button/button.tsx
@@ -0,0 +1,20 @@
+import React, { ButtonHTMLAttributes } from "react";
+import cs from "classnames";
+import styles from "./button.module.scss";
+
+export interface IButton extends ButtonHTMLAttributes<HTMLButtonElement> {
+  /** Choose between primary and secondary styling. */
+  variant: "primary" | "secondary";
+}
+
+export const Button = ({ children, variant = "primary", ...rest }: IButton) => {
+  return (
+    <button className={cs(styles.base, styles[variant])} {...rest}>
+      {children}
+    </button>
+  );
+};
+
+Button.defaultProps = {
+  variant: "primary",
+};
